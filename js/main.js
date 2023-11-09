46b64118982ead3a16ae91ed415ad0b99ae8c9cf
xgiannix <gianni.demare@hotmail.it>
2023-11-09 17:46:26 +0100

    Finished work with bonus

--- js/main.js
+++ js/main.js
@@ -2,25 +2,41 @@
 // INIZIO ESERCIZIO //
 let name = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
-let color = prompt("Inserisci il tuo colore preferito");
-let password = name + surname + color + 21;
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+let password1 = name + surname + favoriteColor + 21;
 
 // PRIMO BONUS // 
 let number = prompt("Inserisci un numero");
 let number2 = prompt("Inserisci un secondo numero");
 number = parseInt(number);
 number2 = parseInt(number2);
-let password2 = name + surname + color + number/number2;
+let password2 = name + surname + favoriteColor + number/number2;
+
+// SECONDO BONUS //
+let password3 = password1 + Math.floor(Math.random() * 101);
 
 let credential = `
 
 Nome: ${name}
 Cognome: ${surname}
-Colore: ${color}
+Colore: ${favoriteColor}
 
-La tua prima password: ${password}
+La tua prima password: ${password1}
 La tua senconda passoword: ${password2}
+La tua terza password: ${password3}
 
 `;
 
 console.log(credential);
+
+document.getElementById("name").innerHTML = name;
+
+document.getElementById("surname").innerHTML = surname;
+
+document.getElementById("favoriteColor").innerHTML = favoriteColor;
+
+document.getElementById("password1").innerHTML = password1;
+
+document.getElementById("password2").innerHTML = password2;
+
+document.getElementById("password3").innerHTML = password3;
